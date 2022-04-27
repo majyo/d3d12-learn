b718274bccdeb6fd2972f3533ef78c17421ef232
majyo <majyoarisu@outlook.com>
2022-04-27 18:32:41 +0800

    init project

---
+++ D3d12Demo/Core.h
@@ -0,0 +1,32 @@
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+#include <Windows.h>
+#include <shellapi.h>
+
+#if defined(min)
+#undef min
+#endif
+
+#if defined(max)
+#undef max
+#endif
+
+#if defined(CreateWindow)
+#undef CreateWindow
+#endif
+
+#include <wrl.h>
+
+#include <d3d12.h>
+#include <dxgi1_6.h>
+#include <d3dcompiler.h>
+#include <DirectXMath.h>
+
+#include "Helpers/d3dx12.h"
+#include "Helpers/Helpers.h"
+
+#include <algorithm>
+#include <cassert>
+#include <chrono>
+
---
+++ D3d12Demo/D3d12Demo.cpp
@@ -0,0 +1,341 @@
+﻿#include "Core.h"
+#include <iostream>
+
+using namespace Microsoft::WRL;
+
+const uint8_t g_NumFrames = 3;
+bool g_UseWarp = false;
+
+uint32_t g_ClientWidth = 1280;
+uint32_t g_ClientHeight = 720;
+
+bool g_IsInitialized = false;
+
+HWND g_hWnd;
+RECT g_WindowRect;
+
+ComPtr<ID3D12Device2> g_Device;
+ComPtr<ID3D12CommandQueue> g_CommandQueue;
+ComPtr<IDXGISwapChain4> g_SwapChain;
+ComPtr<ID3D12Resource> g_BackBuffers[g_NumFrames];
+ComPtr<ID3D12GraphicsCommandList> g_CommandList;
+ComPtr<ID3D12CommandAllocator> g_CommandAllocators[g_NumFrames];
+ComPtr<ID3D12DescriptorHeap> g_RTVDescriptorHeap;
+UINT g_RTVDescriptorSize;
+UINT g_CurrentBackBufferIndex;
+
+ComPtr<ID3D12Fence> g_Fence;
+uint64_t g_FenceValue = 0;
+uint64_t g_FrameFenceValues[g_NumFrames] = {};
+HANDLE g_FenceEvent;
+
+bool g_VSync = true;
+bool g_TearingSupported = false;
+bool g_Fullscreen = false;
+
+LRESULT CALLBACK WndProc(HWND, UINT, WPARAM, LPARAM);
+
+void ParseCommandLineArguments()
+{
+    int argc;
+    wchar_t** argv = ::CommandLineToArgvW(::GetCommandLineW(), &argc);
+
+    for (size_t i = 0; i < argc; ++i)
+    {
+        if (::wcscmp(argv[i], L"-w") == 0 || ::wcscmp(argv[i], L"--width") == 0)
+        {
+            g_ClientWidth = ::wcstol(argv[++i], nullptr, 10);
+        }
+        if (::wcscmp(argv[i], L"-h") == 0 || ::wcscmp(argv[i], L"--height") == 0)
+        {
+            g_ClientHeight = ::wcstol(argv[++i], nullptr, 10);
+        }
+        if (::wcscmp(argv[i], L"-warp") == 0 || ::wcscmp(argv[i], L"--warp") == 0)
+        {
+            g_UseWarp = true;
+        }
+    }
+    ::LocalFree(argv);
+}
+
+void EnableDebugLayer()
+{
+#if defined(_DEBUG)
+    ComPtr<ID3D12Debug> debugInterface;
+    ThrowIfFailed(D3D12GetDebugInterface(IID_PPV_ARGS(&debugInterface)));
+    debugInterface->EnableDebugLayer();
+#endif
+}
+
+void RegisterWindowClass(HINSTANCE hInst, const wchar_t* windowClassName)
+{
+    // Register a window class for creating our render window with.
+    WNDCLASSEXW windowClass = {};
+
+    windowClass.cbSize = sizeof(WNDCLASSEX);
+    windowClass.style = CS_HREDRAW | CS_VREDRAW;
+    windowClass.lpfnWndProc = &WndProc;
+    windowClass.cbClsExtra = 0;
+    windowClass.cbWndExtra = 0;
+    windowClass.hInstance = hInst;
+    windowClass.hIcon = ::LoadIcon(hInst, NULL);
+    windowClass.hCursor = ::LoadCursor(NULL, IDC_ARROW);
+    windowClass.hbrBackground = (HBRUSH)(COLOR_WINDOW + 1);
+    windowClass.lpszMenuName = NULL;
+    windowClass.lpszClassName = windowClassName;
+    windowClass.hIconSm = ::LoadIcon(hInst, NULL);
+
+    static ATOM atom = ::RegisterClassExW(&windowClass);
+    assert(atom > 0);
+}
+
+HWND CreateWindowInstance(const wchar_t* windowClassName, HINSTANCE hInst,
+    const wchar_t* windowTitle, uint32_t width, uint32_t height)
+{
+    int screenWidth = ::GetSystemMetrics(SM_CXSCREEN);
+    int screenHeight = ::GetSystemMetrics(SM_CYSCREEN);
+
+    RECT windowRect = { 0, 0, static_cast<LONG>(width), static_cast<LONG>(height) };
+    ::AdjustWindowRect(&windowRect, WS_OVERLAPPEDWINDOW, FALSE);
+
+    int windowWidth = windowRect.right - windowRect.left;
+    int windowHeight = windowRect.bottom - windowRect.top;
+
+    // Center the window within the screen. Clamp to 0, 0 for the top-left corner.
+    int windowX = std::max<int>(0, (screenWidth - windowWidth) / 2);
+    int windowY = std::max<int>(0, (screenHeight - windowHeight) / 2);
+
+    HWND hWnd = ::CreateWindowExW(
+        NULL,
+        windowClassName,
+        windowTitle,
+        WS_OVERLAPPEDWINDOW,
+        windowX,
+        windowY,
+        windowWidth,
+        windowHeight,
+        NULL,
+        NULL,
+        hInst,
+        nullptr
+    );
+
+    assert(hWnd && "Failed to create window");
+
+    return hWnd;
+}
+
+ComPtr<IDXGIAdapter4> GetAdapter(bool useWarp)
+{
+    ComPtr<IDXGIFactory4> dxgiFactory;
+    UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+    createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+    ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory)));
+
+    ComPtr<IDXGIAdapter1> dxgiAdapter1;
+    ComPtr<IDXGIAdapter4> dxgiAdapter4;
+
+    if (useWarp)
+    {
+        ThrowIfFailed(dxgiFactory->EnumWarpAdapter(IID_PPV_ARGS(&dxgiAdapter1)));
+        ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+    }
+    else
+    {
+        SIZE_T maxDedicatedVideoMemory = 0;
+        for (UINT i = 0; dxgiFactory->EnumAdapters1(i, &dxgiAdapter1) != DXGI_ERROR_NOT_FOUND; ++i)
+        {
+            DXGI_ADAPTER_DESC1 dxgiAdapterDesc1;
+            dxgiAdapter1->GetDesc1(&dxgiAdapterDesc1);
+
+            if ((dxgiAdapterDesc1.Flags & DXGI_ADAPTER_FLAG_SOFTWARE) == 0 &&
+                SUCCEEDED(D3D12CreateDevice(dxgiAdapter1.Get(),
+                    D3D_FEATURE_LEVEL_11_0, __uuidof(ID3D12Device), nullptr)) &&
+                dxgiAdapterDesc1.DedicatedVideoMemory > maxDedicatedVideoMemory)
+            {
+                maxDedicatedVideoMemory = dxgiAdapterDesc1.DedicatedVideoMemory;
+                ThrowIfFailed(dxgiAdapter1.As(&dxgiAdapter4));
+            }
+        }
+    }
+
+    return dxgiAdapter4;
+}
+
+ComPtr<ID3D12Device2> CreateDevice(ComPtr<IDXGIAdapter4> adapter)
+{
+    ComPtr<ID3D12Device2> d3d12Device2;
+    ThrowIfFailed(D3D12CreateDevice(adapter.Get(), D3D_FEATURE_LEVEL_11_0, IID_PPV_ARGS(&d3d12Device2)));
+
+#if defined(_DEBUG)
+    ComPtr<ID3D12InfoQueue> pInfoQueue;
+    if (SUCCEEDED(d3d12Device2.As(&pInfoQueue)))
+    {
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_CORRUPTION, TRUE);
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_ERROR, TRUE);
+        pInfoQueue->SetBreakOnSeverity(D3D12_MESSAGE_SEVERITY_WARNING, TRUE);
+    }
+
+    D3D12_MESSAGE_SEVERITY severities[] = {
+        D3D12_MESSAGE_SEVERITY_INFO
+    };
+
+    D3D12_MESSAGE_ID denyIds[] = {
+    	D3D12_MESSAGE_ID_CLEARRENDERTARGETVIEW_MISMATCHINGCLEARVALUE,
+        D3D12_MESSAGE_ID_MAP_INVALID_NULLRANGE,
+        D3D12_MESSAGE_ID_UNMAP_INVALID_NULLRANGE
+    };
+
+    D3D12_INFO_QUEUE_FILTER NewFilter = {};
+    NewFilter.DenyList.NumSeverities = _countof(severities);
+    NewFilter.DenyList.pSeverityList = severities;
+    NewFilter.DenyList.NumIDs = _countof(denyIds);
+    NewFilter.DenyList.pIDList = denyIds;
+
+    ThrowIfFailed(pInfoQueue->PushStorageFilter(&NewFilter));
+#endif
+
+    return d3d12Device2;
+}
+
+ComPtr<ID3D12CommandQueue> CreateCommandQueue(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)
+{
+    ComPtr<ID3D12CommandQueue> d3d12CommandQueue;
+    D3D12_COMMAND_QUEUE_DESC desc = {};
+    desc.Type = type;
+    desc.Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL;
+    desc.Flags = D3D12_COMMAND_QUEUE_FLAG_NONE;
+    desc.NodeMask = 0;
+
+    ThrowIfFailed(device->CreateCommandQueue(&desc, IID_PPV_ARGS(&d3d12CommandQueue)));
+
+    return d3d12CommandQueue;
+}
+
+bool CheckTearingSupport()
+{
+    BOOL allowTearing = FALSE;
+    ComPtr<IDXGIFactory4> factory4;
+    if (SUCCEEDED(CreateDXGIFactory1(IID_PPV_ARGS(&factory4))))
+    {
+        ComPtr<IDXGIFactory5> factory5;
+        if (SUCCEEDED(factory4.As(&factory5)))
+        {
+	        if (FAILED(factory5->CheckFeatureSupport(
+                DXGI_FEATURE_PRESENT_ALLOW_TEARING,
+                &allowTearing,
+                sizeof(allowTearing)
+            )))
+	        {
+                allowTearing = FALSE;
+	        }
+        }
+    }
+
+    return allowTearing == TRUE;
+}
+
+ComPtr<IDXGISwapChain4> CreateSwapChain(HWND hWnd, ComPtr<ID3D12CommandQueue> commandQueue,
+    uint32_t width, uint32_t height, uint32_t bufferCount)
+{
+    ComPtr<IDXGISwapChain4> dxgiSwapChain4;
+    ComPtr<IDXGIFactory4> dxgiFactory4;
+    UINT createFactoryFlags = 0;
+#if defined(_DEBUG)
+    createFactoryFlags = DXGI_CREATE_FACTORY_DEBUG;
+#endif
+    ThrowIfFailed(CreateDXGIFactory2(createFactoryFlags, IID_PPV_ARGS(&dxgiFactory4)));
+
+    DXGI_SWAP_CHAIN_DESC1 swapChainDesc = {};
+    swapChainDesc.Width = width;
+    swapChainDesc.Height = height;
+    swapChainDesc.Format = DXGI_FORMAT_R8G8B8A8_UNORM;
+    swapChainDesc.Stereo = FALSE;
+    swapChainDesc.SampleDesc = { 1, 0 };
+    swapChainDesc.BufferUsage = DXGI_USAGE_RENDER_TARGET_OUTPUT;
+    swapChainDesc.BufferCount = bufferCount;
+    swapChainDesc.Scaling = DXGI_SCALING_STRETCH;
+    swapChainDesc.SwapEffect = DXGI_SWAP_EFFECT_FLIP_DISCARD;
+    swapChainDesc.AlphaMode = DXGI_ALPHA_MODE_UNSPECIFIED;
+    swapChainDesc.Flags = CheckTearingSupport() ? DXGI_SWAP_CHAIN_FLAG_ALLOW_TEARING : 0;
+
+    ComPtr<IDXGISwapChain1> swapChain1;
+    ThrowIfFailed(dxgiFactory4->CreateSwapChainForHwnd(
+        commandQueue.Get(),
+        hWnd,
+        &swapChainDesc,
+        nullptr,
+        nullptr,
+        &swapChain1
+    ));
+    ThrowIfFailed(dxgiFactory4->MakeWindowAssociation(hWnd, DXGI_MWA_NO_ALT_ENTER));
+    ThrowIfFailed(swapChain1.As(&dxgiSwapChain4));
+
+    return dxgiSwapChain4;
+}
+
+ComPtr<ID3D12DescriptorHeap> CreateDescriptorHeap(ComPtr<ID3D12Device2> device, 
+    D3D12_DESCRIPTOR_HEAP_TYPE type, uint32_t numDescriptors)
+{
+    ComPtr<ID3D12DescriptorHeap> descriptorHeap;
+    D3D12_DESCRIPTOR_HEAP_DESC desc = {};
+    desc.NumDescriptors = numDescriptors;
+    desc.Type = type;
+    ThrowIfFailed(device->CreateDescriptorHeap(&desc, IID_PPV_ARGS(&descriptorHeap)));
+    return descriptorHeap;
+}
+
+void UpdateRenderTargetView(ComPtr<ID3D12Device2> device, ComPtr<IDXGISwapChain4> swapChain,
+    ComPtr<ID3D12DescriptorHeap> descriptorHeap)
+{
+    UINT rtvDescriptorSize = device->GetDescriptorHandleIncrementSize(D3D12_DESCRIPTOR_HEAP_TYPE_RTV);
+    CD3DX12_CPU_DESCRIPTOR_HANDLE rtvHandle(descriptorHeap->GetCPUDescriptorHandleForHeapStart());
+
+    for (size_t i = 0; i < g_NumFrames; ++i)
+    {
+        ComPtr<ID3D12Resource> backBuffer;
+        ThrowIfFailed(swapChain->GetBuffer(i, IID_PPV_ARGS(&backBuffer)));
+        device->CreateRenderTargetView(backBuffer.Get(), nullptr, rtvHandle);
+        g_BackBuffers[i] = backBuffer;
+        rtvHandle.Offset(rtvDescriptorSize);
+    }
+
+}
+
+ComPtr<ID3D12CommandAllocator> CreateCommandAllocator(ComPtr<ID3D12Device2> device, D3D12_COMMAND_LIST_TYPE type)
+{
+    ComPtr<ID3D12CommandAllocator> commandAllocator;
+    ThrowIfFailed(device->CreateCommandAllocator(type, IID_PPV_ARGS(&commandAllocator)));
+    return commandAllocator;
+}
+
+ComPtr<ID3D12GraphicsCommandList> CreateCommandList(ComPtr<ID3D12Device2> device, ComPtr<ID3D12CommandAllocator> commandAllocator, 
+    D3D12_COMMAND_LIST_TYPE type)
+{
+    ComPtr<ID3D12GraphicsCommandList> commandList;
+    ThrowIfFailed(device->CreateCommandList(0, type, commandAllocator.Get(), nullptr, IID_PPV_ARGS(&commandList)));
+    ThrowIfFailed(commandList->Close());
+    return commandList;
+}
+
+ComPtr<ID3D12Fence> CreateFence(ComPtr<ID3D12Device2> device)
+{
+    ComPtr<ID3D12Fence> fence;
+    ThrowIfFailed(device->CreateFence(0, D3D12_FENCE_FLAG_NONE, IID_PPV_ARGS(&fence)));
+    return fence;
+}
+
+HANDLE CreateEventHandle()
+{
+    HANDLE fenceEvent;
+    fenceEvent = ::CreateEvent(NULL, FALSE, FALSE, NULL);
+    assert(fenceEvent && "Failed to create fence event.");
+    return fenceEvent;
+}
+
+int main()
+{
+}
+
