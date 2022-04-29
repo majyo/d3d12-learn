1c94fea35ec0bcbe6578ab778b7474fcf4f54134
majyo <majyoarisu@outlook.com>
2022-04-30 00:18:56 +0800

    add proc functions

--- D3d12Demo/D3d12Demo.cpp
+++ D3d12Demo/D3d12Demo.cpp
@@ -505,8 +505,52 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
             Update();
             Render();
             break;
+		case WM_SYSKEYDOWN:
+		case WM_KEYDOWN:
+			{
+				bool alt = (::GetAsyncKeyState(VK_MENU) & 0x8000) != 0;
+				switch (wParam)
+				{
+				case 'V':
+					g_VSync = !g_VSync;
+					break;
+				case VK_ESCAPE:
+					::PostQuitMessage(0);
+					break;
+				case VK_RETURN:
+					if (alt)
+					{
+				case VK_F11:
+					SetFullscreen(!g_Fullscreen);
+					}
+					break;
+				}
+			}
+            break;
+		case WM_SYSCHAR:
+            break;
+		case WM_SIZE:
+        {
+            RECT clientRect = {};
+            ::GetClientRect(g_hWnd, &clientRect);
+            int width = clientRect.right - clientRect.left;
+            int height = clientRect.bottom - clientRect.top;
+            Resize(width, height);
+        }
+            break;
+		case WM_DESTROY:
+            ::PostQuitMessage(0);
+            break;
+		default:
+            return ::DefWindowProcW(hWnd, message, wParam, lParam);
 		}
 	}
+	else
+	{
+        return ::DefWindowProcW(hWnd, message, wParam, lParam);
+	}
+
+    return 0;
 }
 
 int main()
